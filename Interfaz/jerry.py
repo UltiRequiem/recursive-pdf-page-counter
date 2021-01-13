8b692e896f89fc7dc07bc66df28519f1294f7d08
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-01-12 21:12:25 -0500

    Super AVanze

--- Interfaz/jerry.py
+++ Interfaz/jerry.py
@@ -1,28 +1,39 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QMessageBox
 import os
 from os import path
+
+def show_dialog(title, message):
+    msgBox = QMessageBox()
+    msgBox.setIcon(QMessageBox.Information)
+    msgBox.setText(message)
+    msgBox.setWindowTitle(title)
+    msgBox.show()
+    returnValue = msgBox.exec()
+
+def search_files_in_directory(path, extension):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(extension):
+                print(os.path.join(root, file))
 
 class ejemplo_GUI(QMainWindow):
 
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
-        self.comenzar.clicked.connect(self.read_directory)
-       
+        self.comenzar.clicked.connect(self.start)
 
-    def read_directory(self):       
+    def start(self):              
+        
+        
         user_path = self.folder.toPlainText()
 
         if path.exists(user_path):
-            for root, dirs, files in os.walk(user_path):
-                for file in files:
-                    if file.endswith(".pdf"):
-                        print(os.path.join(root, file))
-
+            search_files_in_directory(user_path, '.pdf')
         else:
-            print('El Directorio no Existe')
+            show_dialog('Alerta', 'El directorio no existe')
 
                 
 
