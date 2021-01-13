d6f87e5ff8a58597182f09cdb34215dc28bba16b
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-01-13 15:30:02 -0500

    PyCharm

--- Source/target/PyInstaller/fbs_pyinstaller_hook.py
+++
@@ -1,3 +0,0 @@
-import importlib
-module = importlib.import_module('fbs_runtime._frozen')
-module.BUILD_SETTINGS = {'app_name': 'PdfToCsv', 'author': 'Eliaz', 'version': '0.0.0', 'environment': 'local'}
