d89f45ea546f1fffb9b6131e42caf215c0aecfa0
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-04-13 16:37:56 -0700

    Update

--- EXE/PyInstaller/fbs_pyinstaller_hook.py
+++
@@ -1,3 +0,0 @@
-import importlib
-module = importlib.import_module('fbs_runtime._frozen')
-module.BUILD_SETTINGS = {'environment': 'local', 'version': '0.0.0', 'author': 'Eliaz', 'app_name': 'PdfToCSV'}
--- EXE/README.md
+++
@@ -1,6 +0,0 @@
-# How to Run?
-The file you must run is:
-
-```PyInstaller/PdfToCSV/PdfToCSV.exe```
-
-But remember that for it to work you must have all the folders and files in the directory **EXE**.
